11fca6c3707a0eb8f5cb6969f94b09a404292438
Larss830 <larence830@gmail.com>
2025-04-04 14:47:30 +0800

    update client data and logo

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,23 @@
 module.exports = {
-    "name": "Yeg Carpet Cleaning Services",
-    "email": "yeg.carpetclean@gmail.com",
-    "phoneForTel": "587-335-2755",
-    "phoneFormatted": "(587) 335-2755",
+    "name": "Dust It Off Cleaning Services",
+    "logo": "https://bespoke-dustitoffcleaning.netlify.app/assets/images/dust-it-off-logo.png",
+    "email": "dustitoffCS@gmail.com",
+    "phoneForTel": "7803946644",
+    "phoneFormatted": "780 394 6644",
     "address": {
-        "lineOne": "10429 32a Ave NW",
-        "lineTwo": "",
+        "lineOne": "2183 Trumpeter Way",
+        "lineTwo": "Edmonton, AB T5S 0K2",
         "city": "Edmonton",
-        "state": "AB",
-        "zip": "T6J 4A7",
-        "country": "Canada",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "state": "Alberta",
+        "zip": "T5S 0K2",
+        "mapLink": "https://maps.app.goo.gl/EMhoYYo3odJMG5bh9"
     },
-    "socials": {
-        "facebook": "https://www.facebook.com/profile.php?id=61565531703551/",
+    "social": {
+        "facebook": "https://www.facebook.com/",
+        "twitter": "https://twitter.com/",
         "instagram": "https://www.instagram.com/",
+        "youtube": "https://www.youtube.com/"
     },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://yegcarpetcleaningservices.ca"
+    "domain": "https://bespoke-dustitoff.netlify.app",
+    "tawkto": "https://embed.tawk.to/67041091cec6d0125df2d037/1i9ju1enk"
 };
